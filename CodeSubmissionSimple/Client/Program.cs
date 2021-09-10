7b14868d5913591270ad45160c902255ac7d35c1
stfeleti <stfeleti@gmail.com>
2021-09-10 15:45:21 +0200

    Checkpoint 2

--- CodeSubmissionSimple/Client/Program.cs
+++ CodeSubmissionSimple/Client/Program.cs
@@ -21,6 +21,7 @@ namespace CodeSubmissionSimple.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<IQuestionDataService, QuestionDataService>();
+            builder.Services.AddScoped<ISubmisstionDataService, SubmissionDataService>();
             await builder.Build().RunAsync();
         }
     }
